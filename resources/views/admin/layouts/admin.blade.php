86b4f0cc9a7f18197544cc95e45ec125a6da0e81
Long-Soeng168 <generalpurpose167@gmail.com>
2025-01-06 11:35:50 +0700

    update

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>One Digital Library</title>
+    <title>CHHAYHOK</title>
 
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
--- resources/views/layouts/isbn.blade.php
+++ resources/views/layouts/isbn.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ISBN Application Form</title>
+    <title>Chhayhok</title>
     <!-- Start CSS -->
     <link rel="stylesheet" href="{{ asset('assets/css/no-tailwind.css') }}" />
 
